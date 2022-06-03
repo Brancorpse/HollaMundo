96d2d5b414da06a288834cb786d12255dac3b13c
brancorpse-admin <lim.machina@gmail.com>
2022-06-03 06:52:58 -0300

    Novos exercicios Curso Python

---
+++ exercicio_listas.py
@@ -0,0 +1,10 @@
+# exercício com listas usando números
+import random
+
+numeros = [random.randrange(10)]
+max = numeros[0]
+print(max)
+for numero in numeros:
+    if numero > max:
+        max = numeros
+print(max)
